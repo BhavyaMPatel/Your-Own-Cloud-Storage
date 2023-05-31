d6760b8db1b2ba490e928dea9e40fa6632160be9
Bhavya Patel <bhavyapatel4012@gmail.com>
2023-05-31 22:43:43 +0530

    Making New Changes And Formatting

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express = require("express");
-var MongoClient = require('mongodb').MongoClient
-var ObjectId=require("mongodb").ObjectId;
-const  Grid  = require("gridfs-stream");
-const { GridFSBucket } = require("mongodb");
+
 const mongoose = require("mongoose");
-const fs = require('fs');
+
 const multer = require("multer");
 const { GridFsStorage } = require("multer-gridfs-storage");
 require('dotenv').config()
 const app = express();
 app.use(express.json());
+
 //to parse body from url
 app.use(express.urlencoded({
   extended: false
 }));
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 mongoose.set('strictQuery', false);
@@ -61,25 +60,17 @@ const upload = multer({
 });
 
 
-
-
-
-
+//Routes
 app.get('/', async (req, res) => {
     let f=[];
     await bucket.find({}).toArray((err,files)=>{
-   
     f=files;
     return res.render("Home",{f,f});
   });
-  // console.log('hi');
-  // console.log(file);
-
 
 });
 
 app.get("/fileinfo/:filename", async(req, res) => {
-    // console.log('ye');
     const file = bucket
       .find({
         filename: req.params.filename
@@ -90,45 +81,24 @@ app.get("/fileinfo/:filename", async(req, res) => {
             .json({
               err: "no files exist"
             });
-            // alert("Error In Database")
         }
         bucket.openDownloadStreamByName(req.params.filename).pipe(res);
-        
       });
-      const uploaded_files = await bucket.find({filename:req.params.filename}).toArray((err,files)=>{
-        // return res.render("Home",{files,files});
-        console.log(files);
-        // console.log(files);
-      });
-
-      
 });
 
 app.post("/upload", upload.any(), (req, res) => {
-
-  res.status(200).redirect("/ok");
+    res.status(200).redirect("/upload/success");
 });
 
 app.post('/delete',async function(req,res){
-  console.log("hi")
-  console.log(req.body.name)
-  
   await bucket.find({filename:req.body.name}).toArray((err,files)=>{
-    
     bucket.delete(files[0]._id);
-  
   });
-  console.log("hi");
   res.status(201).json({user:"ok"})
-  
- 
-    
 })
 
-
-
-app.get('/ok',(req,res)=>{
-  res.send("HI");
+app.get('/upload/success',(req,res)=>{
+  res.send("Upload successful");
 })
 
 const PORT = process.env.PORT | 1000;
